e92830c869073f1f15ebed6ca58513f728bcfea3
hujiajia <1870641142@qq.com>
2023-08-06 21:14:20 +0800

    地址url修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: ' https://hjj1233.github.io/notebook/',
+  url: 'https://hjj1233.github.io/notebook/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
